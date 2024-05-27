bdf004cd4d71366586a3a9d5efd1ec6c3d7e81ce
kris <ssspers92@gmail.com>
2024-05-27 23:43:55 +0300

    Run do() with runserver

--- schedule/apps.py
+++ schedule/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
 
 
+
+
 class ScheduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedule'
 
     def ready(self):
-        from schedule.cron import MyCronJob
-        MyCronJob()
+        from schedule.crontab import do
+        do()
