415213f4f4fe10f3d8c7c3ad2eff4e80476de2fb
kris <ssspers92@gmail.com>
2024-05-27 23:46:21 +0300

    Added selected_clients, selected_messages, status_of_newsletter and selected_start_time to newsletter

--- schedule/views/newsletter.py
+++ schedule/views/newsletter.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from schedule.forms import NewsletterForm
-from schedule.models import Newsletter
+from schedule.models import Newsletter, CREATE
 from schedule.services import send_mailing
 
 
@@ -20,13 +20,16 @@ class NewsletterCreateView(CreateView):
         newsletter = form.save(commit=False)
 
         selected_clients = form.cleaned_data.get('clients')
+        selected_messages = form.cleaned_data.get('message')
+        selected_start_time = form.cleaned_data.get('start_time')
 
-        newsletter.clients.clear()
-
-        for client in selected_clients:
-            newsletter.clients.add(client)
+        newsletter.status_of_newsletter = CREATE
+        newsletter.start_time = selected_start_time
 
         newsletter.save()
+
+        newsletter.clients.set(selected_clients)
+        newsletter.message.set(selected_messages)
 
         send_mailing(newsletter)
 
@@ -44,11 +47,17 @@ class NewsletterUpdateView(UpdateView):
         newsletter = form.save(commit=False)
 
         selected_clients = form.cleaned_data.get('clients')
+        selected_messages = form.cleaned_data.get('message')
+        selected_start_time = form.cleaned_data.get('start_time')
 
         newsletter.clients.clear()
+        newsletter.message.clear()
 
-        for client in selected_clients:
-            newsletter.clients.add(client)
+        newsletter.clients.set(selected_clients)
+        newsletter.message.set(selected_messages)
+
+        newsletter.status_of_newsletter = CREATE
+        newsletter.start_time = selected_start_time
 
         newsletter.save()
 
