aa5ce62f7e847c4bd4b0e39db8024ad5c28d9963
kris <ssspers92@gmail.com>
2024-05-27 23:44:32 +0300

    Change info about status_of_newsletter to CREATE

--- schedule/task_for_mailing.py
+++ schedule/task_for_mailing.py
@@ -1,23 +1,23 @@
-from schedule.models import Newsletter, DAILY, STARTED, WEEKLY, MONTHLY
+from schedule.models import Newsletter, DAILY, CREATE, WEEKLY, MONTHLY
 from schedule.services import send_mailing
 
 
 def create_daily_task():
-    mailings = Newsletter.objects.filter(frequency=DAILY, status_of_newsletter=STARTED)
+    mailings = Newsletter.objects.filter(frequency=DAILY, status_of_newsletter=CREATE)
     if mailings.exists():
         for mailing in mailings:
             send_mailing(mailing)
 
 
 def create_weekly_task():
-    mailings = Newsletter.objects.filter(frequency=WEEKLY, status_of_newsletter=STARTED)
+    mailings = Newsletter.objects.filter(frequency=WEEKLY, status_of_newsletter=CREATE)
     if mailings.exists():
         for mailing in mailings:
             send_mailing(mailing)
 
 
 def create_monthly_task():
-    mailings = Newsletter.objects.filter(frequency=MONTHLY, status_of_newsletter=STARTED)
+    mailings = Newsletter.objects.filter(frequency=MONTHLY, status_of_newsletter=CREATE)
     if mailings.exists():
         for mailing in mailings:
             send_mailing(mailing)
