7b6879b6a20022b9641e96bf06098f509ab04253
kris <ssspers92@gmail.com>
2024-05-28 18:14:29 +0300

    Added include for users

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("schedule.urls", namespace="schedule")),
+    path("users/", include("users.urls", namespace="users")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
