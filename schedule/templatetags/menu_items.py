0dd9e94b894f426f9068536c8eed70a589692b4f
kris <ssspers92@gmail.com>
2024-05-28 20:40:02 +0300

    Created menu for user

---
+++ schedule/templatetags/menu_items.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("schedule/menu_list.html", takes_context=True)
+def get_users_menu(context):
+    request = context.get("request")
+    if request.user.is_authenticated:
+        users_menu = [
+            {"id": 3, "name": "Выйти", "template": "users:logout"},
+        ]
+    else:
+        users_menu = [
+            {"id": 1, "name": "Войти", "template": "users:login"},
+            {"id": 2, "name": "Регистрация", "template": "users:register"},
+        ]
+    return {"users_menu": users_menu}
