5eefcdeb655fe9005cd79622c043a1886e55003b
kris <ssspers92@gmail.com>
2024-05-28 20:58:09 +0300

    Added AUTH_PASSWORD_VALIDATORS, PASSWORD_HASHERS, LOGIN_REDIRECT_URL,  LOGOUT_REDIRECT_URL

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,32 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = Path(BASE_DIR, "media")
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 9,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 587
@@ -167,6 +193,8 @@ if CACHE_ENABLED:
     }
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = "schedule:log_list"
+LOGOUT_REDIRECT_URL = "schedule:log_list"
 
 CRONJOBS = [
     ('*/1 * * * *', 'schedule.crontab.do',
