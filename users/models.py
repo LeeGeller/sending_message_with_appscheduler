c7ea5e4316ae3b7fc6511a64d61870480996ec94
kris <ssspers92@gmail.com>
2024-05-29 17:14:43 +0300

    Created class Company and ForeignKey from User to Company

--- users/models.py
+++ users/models.py
@@ -4,10 +4,27 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Company(models.Model):
+    company_name = models.CharField(max_length=150, verbose_name="Название компании")
+
+    def __str__(self):
+        return self.company_name
+
+    class Mets:
+        verbose_name = "Компания"
+        verbose_name_plural = "Компании"
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(max_length=50, verbose_name="Email", unique=True)
-    company = models.CharField(max_length=200, verbose_name="Компания", null=True)
+    user_company = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        verbose_name="Компания",
+        null=True,
+        blank=True,
+    )
     token = models.CharField(
         max_length=200, verbose_name="Token", blank=True, null=True
     )
