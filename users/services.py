facd110a2835d7d0eacea46596b0c5b87421c8ad
kris <ssspers92@gmail.com>
2024-05-29 16:49:35 +0300

    Added token=token

--- users/services.py
+++ users/services.py
@@ -5,7 +5,7 @@ from users.models import User
 
 
 def email_verification(request, token):
-    user = get_object_or_404(User, token)
+    user = get_object_or_404(User, token=token)
     user.is_active = True
     user.save()
     return redirect(reverse("users:login"))
