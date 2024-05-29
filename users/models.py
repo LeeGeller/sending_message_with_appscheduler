91debb6507b7e4b58b088fc0fec0b1d415bb968e
kris <ssspers92@gmail.com>
2024-05-29 16:49:00 +0300

    Added verbose_name in User in model company

--- users/models.py
+++ users/models.py
@@ -6,13 +6,15 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(
-        max_length=50, verbose_name="Email", help_text="Введите свой email", unique=True
+    email = models.EmailField(max_length=50, verbose_name="Email", unique=True)
+    company = models.CharField(max_length=200, verbose_name="Компания", null=True)
+    token = models.CharField(
+        max_length=200, verbose_name="Token", blank=True, null=True
     )
-    company = models.CharField(max_length=200, verbose_name="", null=True)
-    token = models.CharField(max_length=200, verbose_name="Token", blank=True, null=True)
     is_active = models.BooleanField(default=False)
-    avatar = models.ImageField(verbose_name='Аватар', upload_to='users/', blank=True, null=True)
+    avatar = models.ImageField(
+        verbose_name="Аватар", upload_to="users/", blank=True, null=True
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
