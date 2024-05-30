01e2579091213a7ce79493330b9ce4e01224c8d8
kris <ssspers92@gmail.com>
2024-05-30 18:20:50 +0300

    Added info in list_display

--- schedule/admin.py
+++ schedule/admin.py
@@ -5,20 +5,39 @@ from schedule.models import Client, TextForNewsletter, Newsletter, Log
 
 @admin.register(Client)
 class Client(admin.ModelAdmin):
-    list_display = ('pk', 'contact_email', 'fullname', 'comment',)
+    list_display = (
+        "pk",
+        "contact_email",
+        "fullname",
+        "comment",
+    )
 
 
 @admin.register(TextForNewsletter)
 class TextForNewsletter(admin.ModelAdmin):
-    list_display = ('pk', 'subject', 'text', )
+    list_display = (
+        "pk",
+        "subject",
+        "text",
+    )
 
 
 @admin.register(Newsletter)
 class Newsletter(admin.ModelAdmin):
-    list_display = ('pk', 'start_time', 'end_time', 'frequency', 'status_of_newsletter',)
+    list_display = (
+        "pk",
+        "start_time",
+        "end_time",
+        "frequency",
+        "status_of_newsletter",
+    )
 
 
 @admin.register(Log)
 class Log(admin.ModelAdmin):
     list_display = (
-        'pk', 'status_of_last_attempt', 'server_response', 'time_attempt',)
+        "pk",
+        "status_of_last_attempt",
+        "server_response",
+        "time_attempt",
+    )
