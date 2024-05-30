c407a9bcf9cfed9575ae5717c3dd6405f1d89ae8
kris <ssspers92@gmail.com>
2024-05-30 18:21:56 +0300

    Created view just for owner's users and superusers

--- schedule/views/client.py
+++ schedule/views/client.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
@@ -8,19 +9,46 @@ from schedule.models import Client
 class ClientsListView(LoginRequiredMixin, ListView):
     model = Client
 
+    def get_queryset(self):
+        user = self.request.user
+
+        return Client.objects.filter(company=user.user_company)
+
 
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
-    fields = ('contact_email', 'fullname', 'comment',)
+    fields = (
+        "contact_email",
+        "fullname",
+        "comment",
+    )
     success_url = reverse_lazy("schedule:client_list")
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.save(commit=False)
+        form.instance.company = user.user_company
+
+        return super().form_valid(form)
 
 
 class ClientUpdateView(LoginRequiredMixin, UpdateView):
     model = Client
-    fields = ('contact_email', 'fullname', 'comment',)
+    fields = (
+        "contact_email",
+        "fullname",
+        "comment",
+    )
 
     def get_success_url(self):
         return reverse("schedule:client_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        if not (user.user_company == self.get_object().company or user.is_superuser):
+            return HttpResponseForbidden("Go out!")
+        else:
+            return super().dispatch(request, *args, **kwargs)
 
 
 class ClientDeleteView(LoginRequiredMixin, DeleteView):
@@ -28,3 +56,10 @@ class ClientDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse("schedule:client_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        if not (user.user_company == self.get_object().company or user.is_superuser):
+            return HttpResponseForbidden("Go out!")
+        else:
+            return super().dispatch(request, *args, **kwargs)
