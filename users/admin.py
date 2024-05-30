aefcbee3a9493d57371a8445d7ce900572cf0aae
kris <ssspers92@gmail.com>
2024-05-30 18:21:01 +0300

    Added CompanyAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Company
 
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("pk", "email", "company", "is_active", "avatar")
+    list_display = ("pk", "email", "user_company", "is_active", "avatar")
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "company_name",
+    )
