250bc3f9b98ce7910a068666f44cd49a4cbb6bb2
Kris <ssspers92@gmail.com>
2024-06-07 14:55:19 +0300

    Commit do()

--- schedule/apps.py
+++ schedule/apps.py
@@ -5,7 +5,8 @@ class ScheduleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "schedule"
 
-    def ready(self):
-        from schedule.crontab import do
-
-        do()
+# Чтобы cromtab работал при запуске приложений, нужно будет его раскомитить
+# def ready(self):
+#     from schedule.crontab import do
+#
+#     do()
